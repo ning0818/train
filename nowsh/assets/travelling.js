b3dd261334fb439203d9fac9f10d3c2eced4a4a9
ygblog <117955401+ning0818@users.noreply.github.com>
2022-12-10 15:43:40 +0800

    这个文件里是跳转的链接
    这个文件里是跳转的链接

--- nowsh/assets/travelling.js
+++ nowsh/assets/travelling.js
@@ -2,8 +2,8 @@ function travelling() {
   var url = new Array();
   var rip_url = "https://travellings.link/assets/rip.html";
 
-  url[0] = "https://github.com/volfclub/travelling";
-  url[1] = "https://www.volf.club";
+  url[0] = "www.yuanning0818.tk";
+  url[1] = "www.yuanning0818.tk";
   url[2] = "https://sonic.volf.club";
   url[3] = "https://yltang.cn";
   url[4] = "https://guguga.cn";
